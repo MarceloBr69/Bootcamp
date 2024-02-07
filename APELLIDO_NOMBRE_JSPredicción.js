1f0d13bf4361385135e4a0d7ef469e359ef74e8f
MarceloBr69 <marcelo.burgos200405@gmail.com>
2024-02-07 16:40:33 -0300

    first commit

---
+++ APELLIDO_NOMBRE_JSPredicción.js
@@ -0,0 +1,37 @@
+//Prediccion 1
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }
+
+// Se imprime =
+
+// Nací en 1980
+
+
+
+//Prediccion 2
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+
+// Se imprime = 
+
+// Nací en  1980
+
+
+
+//Prediccion 3
+function add(num1, num2){
+        console.log("¡Sumando números!"); //num 1=10; num2 =20
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+
+// Se imprime =
+
+// ¡Sumando números!
+// num1 is 10
+// num2 is 20
+// 30
