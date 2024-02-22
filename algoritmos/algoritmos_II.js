50d4add01998c0a8540e29aed03244a5131a3a86
MarceloBr69 <marcelo.burgos200405@gmail.com>
2024-02-22 17:11:53 -0300

    Hola

--- algoritmos/algoritmos_II.js
+++ algoritmos/algoritmos_II.js
@@ -9,3 +9,40 @@ console.log(b);
 
 //Se imprime:
 // 2, 3, 2, 3
+
+//Segundo ejercicio
+
+function multiply(x,y) {
+    return x*y;
+}
+b = multiply(2,3);
+console.log(b);
+console.log(multiply(5,2));
+
+//Se imprime:
+//6, 10
+
+//Tercer ejercicio
+
+var x = [1,2,3,4,5,10];
+for(var i=0; i<5; i++) {
+   i = i + 3; 
+   console.log(i);
+}
+
+//Se imprime:
+//3, 7
+
+//Cuarto ejercicio
+var x=15;
+console.log(x);
+function awesome(){
+    var x=10;
+    console.log(x);
+}
+console.log(x);
+awesome();
+console.log(x);
+
+//Se imprime:
+//15
